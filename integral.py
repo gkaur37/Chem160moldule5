843e959900529fb6b330a98391c96995ff20f340
gkaur37 <54698652+gkaur37@users.noreply.github.com>
2019-09-22 18:57:44 -0700

    Add files via upload

---
+++ integral.py
@@ -0,0 +1,14 @@
+def prints(x):
+     print(x)
+y=10
+prints(y) ## Inside prints() the value in y is assigned to x
+
+from math import exp
+def f(x):
+    return x*exp(-x)
+f(0.1)
+f(10)
+
+#f(0.1)= 0.09048374180359596
+#f(10)= 0.0004539999299762484856
+
